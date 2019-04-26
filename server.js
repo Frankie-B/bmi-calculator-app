b3f1135ae8b9e1f3cbd87d376e5dd5e3a735bdb0
Frankie Bukenya <frankie.bukenya@gmail.com>
2019-04-26 08:36:06 +0300

    Add BMI Functionality

--- server.js
+++ server.js
@@ -12,6 +12,17 @@ app.use(express.json());
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
+
+// Calculating the BMI and procesing the POST request
+app.post('/', function(req, res) {
+  var height = Number(req.body.height);
+  var weight = Number(req.body.weight);
+
+  var bmi = Math.floor(weight / (height * height));
+
+  // What is returned to the user on submit
+  res.send('Your BMI is ' + bmi);
+});
 // Spin up server on port 3000
 app.listen(3000, function() {
   console.log('Server started on port 3000');
